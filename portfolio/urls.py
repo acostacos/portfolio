66558e3b341a905fbffacdad9fcee5addbe7e4fe
Carlo Acosta <acostacarlom@gmail.com>
2024-12-05 22:19:45 +0800

    Added apps for portfolio: info, blog, projects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from portfolio.views import index, about
+from django.urls import path, include
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("about/", about, name="about"),
+    path('', include('info.urls')),
     path('admin/', admin.site.urls),
 ]
